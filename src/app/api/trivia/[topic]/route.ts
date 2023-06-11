9bab257ca4ca656b0c2a9a659c841d4269e16367
miguelangellerma <miguellangel770@gmail.com>
2023-06-10 22:46:52 -0500

    Moved trivia route to dynamic route folder

---
+++ src/app/api/trivia/[topic]/route.ts
@@ -0,0 +1,16 @@
+import { generateTrivia } from '@/trivia/generateTrivia'
+import { TriviaTopic } from '@/types'
+import { NextRequest, NextResponse } from 'next/server'
+
+type Params = {
+	params: {
+		topic: string
+	}
+}
+
+export const GET = async (req: NextRequest, { params }: Params) => {
+	const topic = params.topic as TriviaTopic
+	const response = await generateTrivia(topic || 'Entertainment')
+
+	return NextResponse.json({ ...response })
+}
