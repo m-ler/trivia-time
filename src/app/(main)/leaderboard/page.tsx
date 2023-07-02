648c830fe4d4dd9069006ebe4edae565a39cf76f
miguelangellerma <miguellangel770@gmail.com>
2023-07-02 01:07:32 -0500

    Updated leaderboard fetch error handling

--- src/app/(main)/leaderboard/page.tsx
+++ src/app/(main)/leaderboard/page.tsx
@@ -4,27 +4,26 @@ import { UserWithProfile } from '@/types'
 
 export const metadata = {
 	title: 'Leaderboard',
-} 
+}
 
 const getLeaderboardData = async () => {
 	const res = await fetch(`${NEXTAUTH_URL}/api/leaderboard`, {
 		next: {
 			revalidate: 60,
-		}, 
+		},
 	})
 
 	if (!res.ok) {
-		throw new Error('Failed to obtain leaderboard data')
+		return []
 	}
 
 	const result = await res.json()
 	return result as UserWithProfile[]
 }
 
-const Page = async () => { 
+const Page = async () => {
 	const data = await getLeaderboardData()
 	return <Leaderboard data={data || []} />
 }
 
 export default Page
-  
