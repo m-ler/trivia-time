7cd0ecf8bc4be3e4dafd44cbfa75a76625d639fa
miguelangellerma <miguellangel770@gmail.com>
2023-07-02 15:41:47 -0500

    Added revalidate segment config in leaderboard route handler

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma/client'
 import { NextResponse } from 'next/server'
+export const revalidate = 60
 
 export const GET = async () => {
 	try {
@@ -27,7 +28,7 @@ export const GET = async () => {
 			},
 			take: 100,
 		})
-		return NextResponse.json(users)
+		return NextResponse.json(users, {})
 	} catch (e) {
 		return new NextResponse('', { status: 500 })
 	}
