544514b1b76b130526e73434012e6496e83676e2
miguelangellerma <miguellangel770@gmail.com>
2023-07-01 21:22:09 -0500

    Removed fake delay in trivia topic query

--- src/app/api/trivia/[topic]/route.ts
+++ src/app/api/trivia/[topic]/route.ts
@@ -16,7 +16,6 @@ export const POST = async (req: NextRequest, { params }: Params) => {
 	const topic = params.topic as TriviaTopic
 	const body: BodyParams = await req.json()
 	const { apiKey } = body
-	await new Promise((resolve) => setTimeout(resolve, 8000))
 	const response = await generateTrivia(topic || 'Entertainment', apiKey)
 
 	return NextResponse.json({ ...response })
