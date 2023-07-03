9fffcb4eb2788b67cd9fdb1a1bf795f6569495c9
miguelangellerma <miguellangel770@gmail.com>
2023-07-02 20:31:29 -0500

    Adjusted home page's play button link accesibility

--- src/app/(main)/Home.tsx
+++ src/app/(main)/Home.tsx
@@ -26,12 +26,12 @@ const Home = () => {
 						<Text as="h3" color="white" fontSize={16} mt={4} display="block">
 							Put your knowledge to the test with AI-generated trivias, compete with people around the world, climb the
 							leaderboard, and become a true trivia master.
-							<Link href="/play" style={{ display: 'block', width: 'fit-content', height: 'fit-content' }}>
-								<Button colorScheme="yellow" mt={8} mr="auto" size="lg" rightIcon={<MdPlayCircleFilled size={22} />}>
-									START
-								</Button>
-							</Link>
 						</Text>
+						<Link href="/play" style={{ display: 'block', width: 'fit-content', height: 'fit-content' }} aria-label='Go to play page'>
+							<Button colorScheme="yellow" mt={8} mr="auto" size="lg" rightIcon={<MdPlayCircleFilled size={22} />}>
+								START
+							</Button>
+						</Link>
 					</Flex>
 					<Flex>
 						<Image
