a72a4f7240a17d4f62f54fb20b1ffbe440bd97f7
miguelangellerma <miguellangel770@gmail.com>
2023-07-03 14:55:02 -0500

    Added Open Graph and Twitter metadata

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,6 +4,25 @@ export const metadata = {
 	title: 'Trivia Time! | Play AI-generated trivias',
 	description:
 		'Play AI-generated trivias and explore the fascinating worlds of art, entertainment, geography, history, and science. Immerse yourself in captivating quizzes that will test your knowledge across a wide range of subjects. Compete with people around the world, climb the leaderboard, and become a true trivia master.',
+	openGraph: {
+		title: 'Trivia Time!',
+		type: 'website',
+		siteName: 'Trivia Time!',
+		description: 'Play AI-generated trivias.',
+		images: [
+			{
+				url: '/img/trivia_time_logo_512.png',
+				width: 512,
+				height: 512,
+			},
+		],
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Trivia Time!',
+		description: 'Play AI-generated trivias.',
+		images: ['/img/trivia_time_logo_512.png'],
+	},
 }
 
 const Page = () => {
